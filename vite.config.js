1cece35cca07ea42561f49c76d65a4e20850bc89
dhineshwebdeveloper <dhineshwebdeveloper90@gmail.com>
2025-01-08 09:05:14 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'path': false,
-      'http': false,
-      'url': false,
-      'buffer': false,
-    },
-  },
-})
+ server: {
+  host:true,
+  port:80,
+ },
+});
